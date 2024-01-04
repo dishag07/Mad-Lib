d0679948e5989261908c01a9b50c849aa9aa3ace
dishag07 <144719310+dishag07@users.noreply.github.com>
2024-01-04 00:05:04 +0000

    trial

--- script.js
+++ script.js
@@ -2,8 +2,9 @@ let adjective = prompt('Enter an adjective:')
 let noun = prompt('Enter an noun:')
 let verb = prompt('Enter an verb:')
 let adverb = prompt('Enter an adverb:')
+let relative = prompt ('Enter a relationship')
+let place = prompt ('Enter a place')
 
-let story =  `Once Upon a Time, in a ${adjective} ${noun} ${adverb} ${verb}.`
-
+let story = `Dear Santa... I have been a very ${adjective} ${noun}. This year I helped my ${relative} with chores around the ${place}. `
 let madLibOutputDiv = document.getElementById('madLibOutput')
 madLibOutputDiv.innerHTML = `<p>${story}</p>`
